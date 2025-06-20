4d865410d8ce0f441dc8c9ff448dfbff15915cd6
Denisson Pereira Santos <denisson.pereira753@gmail.com>
2025-06-20 10:41:41 -0300

    create project

---
+++ src/main/java/com/denisson/stokpro/StokproApplication.java
@@ -0,0 +1,13 @@
+package com.denisson.stokpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StokproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StokproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/denisson/stokpro/StokproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.denisson.stokpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StokproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
